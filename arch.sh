2689055f4e382e88789a5e5bc9870d9cbf917ec8
bastelfreak <tim@bastelfreak.de>
2016-01-20 18:04:33 +0100

    validate against shellcheck
    this changes just style stuff like:
    * quotation
    * indentation
    * trailing whitespace
    * adding comments

--- arch.sh
+++ arch.sh
@@ -6,43 +6,45 @@
 # originally written by Markus Schade
 # (c) 2013-2015, Hetzner Online GmbH
 #
-
+# changed and extended by Thore Bödecker, 2015-10-05
+# changed and extended by Tim Meusel
+#
 
 # setup_network_config "$device" "$HWADDR" "$IPADDR" "$BROADCAST" "$SUBNETMASK" "$GATEWAY" "$NETWORK" "$IP6ADDR" "$IP6PREFLEN" "$IP6GATEWAY"
 setup_network_config() {
-  if [ "$1" -a "$2" ]; then
+  if [ -n "$1" ] && [ -n "$2" ]; then
     # good we have a device and a MAC
     CONFIGFILE="$FOLD/hdd/etc/systemd/network/50-hetzner.network"
     UDEVFILE="$FOLD/hdd/etc/udev/rules.d/80-net-setup-link.rules"
 
-    echo -e "### Hetzner Online GmbH - installimage" > $UDEVFILE
-    echo -e "# device: $1" >> $UDEVFILE
-    echo -e "SUBSYSTEM==\"net\", ACTION==\"add\", DRIVERS==\"?*\", ATTR{address}==\"$2\", ATTR{dev_id}==\"0x0\", ATTR{type}==\"1\", KERNEL==\"eth*\", NAME=\"$1\"" >> $UDEVFILE
+    echo -e "### ${COMPANY} - installimage" > "$UDEVFILE"
+    echo -e "# device: $1" >> "$UDEVFILE"
+    printf 'SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="%s", ATTR{dev_id}=="0x0", ATTR{type}=="1", KERNEL=="eth*", NAME="%s"\n' "$2" "$1" >> "$UDEVFILE"
 
-    echo -e "### Hetzner Online GmbH - installimage" > $CONFIGFILE
-    echo -e "# device: $1" >> $CONFIGFILE
-    echo -e "[Match]" >> $CONFIGFILE
-    echo -e "MACAddress=$2" >> $CONFIGFILE
-    echo -e "" >> $CONFIGFILE
+    echo -e "### $COMPANY - installimage" > "$CONFIGFILE"
+    echo -e "# device: $1" >> "$CONFIGFILE"
+    echo -e "[Match]" >> "$CONFIGFILE"
+    echo -e "MACAddress=$2" >> "$CONFIGFILE"
+    echo -e "" >> "$CONFIGFILE"
 
-    echo -e "[Network]" >> $CONFIGFILE
-    if [ "$8" -a "$9" -a "${10}" ]; then
+    echo -e "[Network]" >> "$CONFIGFILE"
+    if [ -n "$8" ] && [ -n "$9" ] && [ -n "${10}" ]; then
       debug "setting up ipv6 networking $8/$9 via ${10}"
-      echo -e "Address=$8/$9" >> $CONFIGFILE
-      echo -e "Gateway=${10}" >> $CONFIGFILE
-      echo -e "" >> $CONFIGFILE
+      echo -e "Address=$8/$9" >> "$CONFIGFILE"
+      echo -e "Gateway=${10}" >> "$CONFIGFILE"
+      echo -e "" >> "$CONFIGFILE"
     fi
 
-    if [ "$3" -a "$4" -a "$5" -a "$6" -a "$7" ]; then
+    if [ -n "$3" ] && [ -n "$4" ] && [ -n "$5" ] && [ -n "$6" ] && [ -n "$7" ]; then
       debug "setting up ipv4 networking $3/$5 via $6"
-      echo -e "Address=$3/$CIDR" >> $CONFIGFILE
-      echo -e "Gateway=$6" >> $CONFIGFILE
-      echo -e "" >> $CONFIGFILE
+      echo -e "Address=$3/$CIDR" >> "$CONFIGFILE"
+      echo -e "Gateway=$6" >> "$CONFIGFILE"
+      echo -e "" >> "$CONFIGFILE"
 
       if ! is_private_ip "$3"; then
-        echo -e "[Route]" >> $CONFIGFILE
-        echo -e "Destination=$7/$CIDR" >> $CONFIGFILE
-        echo -e "Gateway=$6" >> $CONFIGFILE
+        echo -e "[Route]" >> "$CONFIGFILE"
+        echo -e "Destination=$7/$CIDR" >> "$CONFIGFILE"
+        echo -e "Gateway=$6" >> "$CONFIGFILE"
       fi
     fi
 
@@ -56,8 +58,8 @@ setup_network_config() {
 generate_config_mdadm() {
   if [ "$1" ]; then
     MDADMCONF="/etc/mdadm.conf"
-    echo "DEVICES /dev/[hs]d*" > $FOLD/hdd$MDADMCONF
-    echo "MAILADDR root" >> $FOLD/hdd$MDADMCONF
+    echo "DEVICES /dev/[hs]d*" > "$FOLD/hdd$MDADMCONF"
+    echo "MAILADDR root" >> "$FOLD/hdd$MDADMCONF"
     execute_chroot_command "mdadm --examine --scan >> $MDADMCONF"; EXITCODE=$?
     return $EXITCODE
   fi
@@ -68,16 +70,16 @@ generate_config_mdadm() {
 generate_new_ramdisk() {
   if [ "$1" ]; then
     local blacklist_conf="$FOLD/hdd/etc/modprobe.d/blacklist-hetzner.conf"
-    echo -e "### Hetzner Online GmbH - installimage" > $blacklist_conf
-    echo -e "### silence any onboard speaker" >> $blacklist_conf
-    echo -e "blacklist pcspkr" >> $blacklist_conf
-    echo -e "blacklist snd_pcsp" >> $blacklist_conf
-    echo -e "### i915 driver blacklisted due to various bugs" >> $blacklist_conf
-    echo -e "### especially in combination with nomodeset" >> $blacklist_conf
-    echo -e "blacklist i915" >> $blacklist_conf
-    echo -e "### mei driver blacklisted due to serious bugs" >> $blacklist_conf
-    echo -e "blacklist mei" >> $blacklist_conf
-    echo -e "blacklist mei-me" >> $blacklist_conf
+    echo -e "### $COMPANY - installimage" > "$blacklist_conf"
+    echo -e "### silence any onboard speaker" >> "$blacklist_conf"
+    echo -e "blacklist pcspkr" >> "$blacklist_conf"
+    echo -e "blacklist snd_pcsp" >> "$blacklist_conf"
+    echo -e "### i915 driver blacklisted due to various bugs" >> "$blacklist_conf"
+    echo -e "### especially in combination with nomodeset" >> "$blacklist_conf"
+    echo -e "blacklist i915" >> "$blacklist_conf"
+    echo -e "### mei driver blacklisted due to serious bugs" >> "$blacklist_conf"
+    echo -e "blacklist mei" >> "$blacklist_conf"
+    echo -e "blacklist mei-me" >> "$blacklist_conf"
 
     execute_chroot_command 'sed -i /etc/mkinitcpio.conf -e "s/^HOOKS=.*/HOOKS=\"base udev autodetect modconf block mdadm lvm2 filesystems keyboard fsck\"/"'
     execute_chroot_command "mkinitcpio -p linux"; EXITCODE=$?
@@ -90,7 +92,7 @@ setup_cpufreq() {
   if [ "$1" ]; then
     if ! isVServer; then
       CPUFREQCONF="$FOLD/hdd/etc/default/cpupower"
-      sed -i -e "s/#governor=.*/governor'$1'/" $CPUFREQCONF
+      sed -i -e "s/#governor=.*/governor'$1'/" "$CPUFREQCONF"
       execute_chroot_command "systemctl enable cpupower"
     fi
 
@@ -152,24 +154,24 @@ validate_image() {
 
 # extract image file to hdd
 extract_image() {
-  LANG=C pacstrap -m -a $FOLD/hdd base btrfs-progs cpupower cronie findutils gptfdisk grub haveged openssh vim wget 2>&1 | debugoutput
+  LANG=C pacstrap -m -a "$FOLD/hdd" base btrfs-progs cpupower cronie findutils gptfdisk grub haveged openssh vim wget 2>&1 | debugoutput
 
   if [ "$EXITCODE" -eq "0" ]; then
     cp -r "$FOLD/fstab" "$FOLD/hdd/etc/fstab" 2>&1 | debugoutput
 
     #timezone - we are in Germany
     execute_chroot_command "ln -s /usr/share/timezone/Europe/Berlin /etc/localtime"
-    echo en_US.UTF-8 UTF-8 > $FOLD/hdd/etc/locale.gen
-    echo de_DE.UTF-8 UTF-8 >> $FOLD/hdd/etc/locale.gen
+    echo en_US.UTF-8 UTF-8 > "$FOLD/hdd/etc/locale.gen"
+    echo de_DE.UTF-8 UTF-8 >> "$FOLD/hdd/etc/locale.gen"
     execute_chroot_command "locale-gen"
 
-    echo > $FOLD/hdd/etc/locale.conf
-    echo "LANG=de_DE.UTF-8" >> $FOLD/hdd/etc/locale.conf
-    echo "LC_MESSAGES=C" >> $FOLD/hdd/etc/locale.conf
+    echo > "$FOLD/hdd/etc/locale.conf"
+    echo "LANG=de_DE.UTF-8" >> "$FOLD/hdd/etc/locale.conf"
+    echo "LC_MESSAGES=C" >> "$FOLD/hdd/etc/locale.conf"
 
-    echo > $FOLD/hdd/etc/vconsole.conf
-    echo "KEYMAP=de" >> $FOLD/hdd/etc/vconsole.conf
-    echo "FONT=LatArCyrHeb-16" >> $FOLD/hdd/etc/vconsole.conf
+    echo > "$FOLD/hdd/etc/vconsole.conf"
+    echo "KEYMAP=de" >> "$FOLD/hdd/etc/vconsole.conf"
+    echo "FONT=LatArCyrHeb-16" >> "$FOLD/hdd/etc/vconsole.conf"
 
 
     return 0
