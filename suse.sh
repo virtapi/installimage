70d62894b7357751c4e3505a516c46859c505644
Tim Meusel <tim@bastelfreak.de>
2016-02-16 23:35:18 +0100

    fix SC2166 and SC2143 in suse.sh
    Prefer [ p ] && [ q ] as [ p -a q ] is not well defined. [SC2166]
    Use grep -q instead of comparing output with [ -n .. ]. [SC2143]

--- suse.sh
+++ suse.sh
@@ -252,7 +252,7 @@ generate_config_grub() {
       grub_linux_default="${grub_linux_default} elevater=noop"
     fi
     # H8SGL need workaround for iommu
-    if [ -n "$(dmidecode -s baseboard-product-name | grep -i h8sgl)" -a $IMG_VERSION -ge 131 ] ; then
+    if dmidecode -s baseboard-product-name | grep -q -i h8sgl && [ $IMG_VERSION -ge 131 ] ; then
       grub_linux_default="${grub_linux_default} iommu=noaperture"
     fi
 
