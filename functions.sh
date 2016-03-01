61844aeffafdac67d3e129f92798c75238deb62c
Tim Meusel <tim@bastelfreak.de>
2016-03-01 14:05:27 +0100

    check if /proc/mdstat is present

--- functions.sh
+++ functions.sh
@@ -1437,9 +1437,11 @@ stop_lvm_raid() {
 
   dmsetup remove_all
 
-  test -x "$(which mdadm)" && grep md /proc/mdstat | cut -d ' ' -f1 | while read -r i; do
-    [ -e "/dev/$i" ] && mdadm -S "/dev/$i" >> /dev/null 2>&1
-  done
+  if [ -x "$(which mdadm)" ] && [ -f /proc/mdstat ]; then
+    grep md /proc/mdstat | cut -d ' ' -f1 | while read -r i; do
+      [ -e "/dev/$i" ] && mdadm -S "/dev/$i" >> /dev/null 2>&1
+    done
+  fi
 }
 
 
